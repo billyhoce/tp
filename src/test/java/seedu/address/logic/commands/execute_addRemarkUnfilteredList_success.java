e4d6645382437509bfb51a8b984f3636ec8860e4
billyhoce <billyhoce@gmail.com>
2024-02-28 20:44:40 +0800

    Add support for new Remark command

---
+++ src/test/java/seedu/address/logic/commands/execute_addRemarkUnfilteredList_success.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class execute_addRemarkUnfilteredList_success {
+
+    @Test
+    void execute() {
+    }
+}
